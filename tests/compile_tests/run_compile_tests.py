46e2e714cae526ed2078b7b1710b15e0d03a97dd
Mohammadreza Yaghoobi <yaghoobi@lh-login1.arc-ts.umich.edu>
2020-01-15 18:00:10 -0500

    modifying the test python

--- tests/compile_tests/run_compile_tests.py
+++ tests/compile_tests/run_compile_tests.py
@@ -8,15 +8,14 @@ import sys
 import os,subprocess,shutil,fnmatch
 import re,itertools
 
+np.seterr(divide='ignore', invalid='ignore')
+
 if( len(sys.argv) - 1 > 0 ):
     numprocs = int(sys.argv[1])
 else:
     numprocs = 1
 
-#ncols_totest = 12
-strainError = 10^-7
-stressError = 10^-5
-slipError = 10^-6
+Error = float(1e-2)
 
 rootDirectory = os.getcwd()
 applicationsDirectory = rootDirectory+'/../../applications/crystalPlasticity'
@@ -30,22 +29,42 @@ testDirectories = ['/fcc/simpleTension/'
                   ,'/fcc/simpleTension_ExternalMesh/'
                   ,'/hcp/compression/'
                   ,'/hcp/simpleTension/'
-                  ]
+                  ] 
 
 for item in testDirectories:
 
-    #runtimeOutputFile = applicationsDirectory+item+'testing.out'
+    runtimeOutputFile = rootDirectory+item+'testing.out'
     os.chdir(applicationsDirectory+item)
 
-    #subprocess.call(['mpirun -np %d ../../main prm.in' %numprocs],stdout=open(runtimeOutputFile,'w'), stderr=subprocess.STDOUT,shell=True)
-    subprocess.call('mpirun -np %d ../../main prm.in' %numprocs, shell=True)
+    subprocess.call(['mpirun -np %d ../../main prm.in' %numprocs],stdout=open(runtimeOutputFile,'w'), stderr=subprocess.STDOUT,shell=True)
+
     old_stress_strain = np.loadtxt(rootDirectory+item+'correct_stressstrain.txt',skiprows=1)
     new_stress_strain = np.loadtxt(applicationsDirectory+item+'results/stressstrain.txt',skiprows=1)
 
-    if((np.max(np.abs(old_stress_strain[0:999,:6] - new_stress_strain[0:999,:6]))<strainError) and (np.max(np.abs(old_stress_strain[0:999,6:12] - new_stress_strain[0:999,6:12]))<stressError) and (np.max(np.abs(old_stress_strain[0:999,12:15] - new_stress_strain[0:999,12:15]))<slipError)):
+    old_vonmisesStrain=np.sqrt(0.5*(np.power(old_stress_strain[:,0]-old_stress_strain[:,1],2)+np.power(old_stress_strain[:,1]-old_stress_strain[:,2],2)+np.power(old_stress_strain[:,2]-old_stress_strain[:,0],2)+6*(np.power(old_stress_strain[:,3],2)+np.power(old_stress_strain[:,4],2)+np.power(old_stress_strain[:,5],2))))
+    old_vonmisesStress=np.sqrt(0.5*(np.power(old_stress_strain[:,6]-old_stress_strain[:,7],2)+np.power(old_stress_strain[:,7]-old_stress_strain[:,8],2)+np.power(old_stress_strain[:,8]-old_stress_strain[:,6],2)+6*(np.power(old_stress_strain[:,9],2)+np.power(old_stress_strain[:,10],2)+np.power(old_stress_strain[:,11],2))))
+    new_vonmisesStrain=np.sqrt(0.5*(np.power(new_stress_strain[:,0]-new_stress_strain[:,1],2)+np.power(new_stress_strain[:,1]-new_stress_strain[:,2],2)+np.power(new_stress_strain[:,2]-new_stress_strain[:,0],2)+6*(np.power(new_stress_strain[:,3],2)+np.power(new_stress_strain[:,4],2)+np.power(new_stress_strain[:,5],2))))
+    new_vonmisesStress=np.sqrt(0.5*(np.power(new_stress_strain[:,6]-new_stress_strain[:,7],2)+np.power(new_stress_strain[:,7]-new_stress_strain[:,8],2)+np.power(new_stress_strain[:,8]-new_stress_strain[:,6],2)+6*(np.power(new_stress_strain[:,9],2)+np.power(new_stress_strain[:,10],2)+np.power(new_stress_strain[:,11],2))))
+
+    if (item=='/hcp/compression/' or item=='/hcp/simpleTension/'):
+        CheckNumber=500
+    else:
+        CheckNumber=1000
+
+    CheckStrainError=np.max(np.nan_to_num(np.abs((old_vonmisesStrain[0:CheckNumber]-new_vonmisesStrain[0:CheckNumber])/old_vonmisesStrain[0:CheckNumber])))
+    CheckStressError=np.max(np.nan_to_num(np.abs((old_vonmisesStress[0:CheckNumber]-new_vonmisesStress[0:CheckNumber])/old_vonmisesStress[0:CheckNumber])))
+    CheckTwinError=np.max(np.nan_to_num(np.abs((old_stress_strain[0:CheckNumber,13]-new_stress_strain[0:CheckNumber,13])/old_stress_strain[0:CheckNumber,13])))
+    CheckSlipError=np.max(np.nan_to_num(np.abs((old_stress_strain[0:CheckNumber,14]-new_stress_strain[0:CheckNumber,14])/old_stress_strain[0:CheckNumber,14])))
+
+    if(CheckStressError<Error and CheckStrainError<Error and CheckSlipError<Error):
         print('%s worked successfully\n' %item)
+        print('%f Stress Relative Error\n' %CheckStressError)
+        print('%f Strain Relative Error\n' %CheckStrainError)
+        print('%f Twin Volume Relative Error\n' %CheckTwinError)
+        print('%f Slip Relative Error\n' %CheckSlipError)
     else:
         print('%s failed\n' %item)
-        break
-
-    #os.chdir('..')
+        print('%f Stress Relative Error\n' %CheckStressError)
+        print('%f Strain Relative Error\n' %CheckStrainError)
+        print('%f Twin Volume Relative Error\n' %CheckTwinError)
+        print('%f Slip Relative Error\n' %CheckSlipError)
